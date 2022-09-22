e7dedd615f4c886c81b3e01e74658e9d01ebdb7a
NonsoGodson <onyedumekwunonso@yahoo.com>
2022-09-22 10:00:46 -0700

    commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
+from calculator_1 import add, sub, mul, div
+
+
+def arg_calc(argv):
+    n = len(argv) - 1
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    if operator == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
+    elif operator == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, sub(a, b)))
+    elif operator == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, mul(a, b)))
+    elif operator == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] is "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        print("Unknown operatorerator. Available operators: +, -, * and /")
+        exit(1)
+
+
+if __name__ == "__main__":
+    from sys import argv
+    arg_calc(argv)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,17 @@
-	#!/usr/bin/python3
+#!/usr/bin/python3
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-    print("{:d}".format(res))
+    from sys import argv
+    add_arg(argv)
