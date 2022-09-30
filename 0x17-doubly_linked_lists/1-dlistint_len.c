b0bb8e054dc3fa01e88b2af66a85928fee46b408
NonsoGodson <onyedumekwunonso@yahoo.com>
2022-09-29 21:24:29 -0700

    commit

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,17 @@
 #include "lists.h"
-
 /**
- * dlistint_len - count all the elements of a dlistint_t list
- * @h: doubly linked list node
- * Return: the number of nodes
+ * dlistint_len - show us the number of elements in a DLL
+ * @h: a pointer to the head of the DLL
+ * Return: the number of elements in a DLL
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t size = 0;
-
-	for (; h; h = h->next, size++)
-		;
-	return (size);
+	size_t num_of_elements;
+	num_of_elements = 0;
+	while (h)
+	{
+		h = h->next;
+		num_of_elements = num_of_elements + 1;
+	}
+	return (num_of_elements);
 }
